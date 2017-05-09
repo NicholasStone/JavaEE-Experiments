83d8471453e4d0b40d41a55f4ee9717425f49e70
Nicholas-Stone <Nicholas-Stone@outlook.com>
2017-05-09 17:42:22 +0800

    Struts2 start

--- experiment-2/src/main/java/com/twodogs/action/AuthAction.java
+++ experiment-2/src/main/java/com/twodogs/action/AuthAction.java
@@ -1,15 +1,47 @@
 package com.twodogs.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import javax.xml.ws.Action;
 import java.util.List;
 
 /**
  * Created by nicholas on 17-5-2.
  */
 public class AuthAction extends ActionSupport {
-    @Override
-    public String execute() throws Exception {
+    private String  password;
+    private String  username;
+    private boolean identity;
+
+    public boolean isIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(boolean identity) {
+        this.identity = identity;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String auth() throws Exception {
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(identity);
         return SUCCESS;
     }
 }
